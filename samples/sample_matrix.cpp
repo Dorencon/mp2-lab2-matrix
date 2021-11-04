c3e6238c1c28c164fc2180b65474262b9ac6d2ec
dorencon <dorfilfilatov@yandex.ru>
2021-11-04 19:09:53 +0300

    I think it's done

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -7,25 +7,54 @@
 
 #include <iostream>
 #include "utmatrix.h"
+#include <string>
 //---------------------------------------------------------------------------
 
 void main()
 {
-  TMatrix<int> a(5), b(5), c(5);
-  int i, j;
-
   setlocale(LC_ALL, "Russian");
   cout << "Тестирование программ поддержки представления треугольных матриц"
     << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
-    {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
-    }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+	cout << "Введите размер матриц" << endl;
+	int n;
+	cin >> n;
+	TMatrix<double> a(n), b(n);
+	cout << "Введите элементы" << endl;
+	cin >> a;
+	cin >> b;
+	while (true)
+	{
+		cout << "Введите операцию" << endl;
+		string s;
+		cin >> s;
+		if (s == "STOP")
+		{
+			break;
+		}
+		else if (s == "+")
+		{
+			cout << a + b;
+		}
+		else if (s == "-")
+		{
+			cout << a - b;
+		}
+		else if (s == "==")
+		{
+			cout << (a == b) << endl;
+		}
+		else if (s == "=>")
+		{
+			a = b;
+		}
+		else if (s == "<=")
+		{
+			b = a;
+		}
+		else
+		{
+			cout << "Неизвестная операция" << endl;
+		}
+	}
 }
 //---------------------------------------------------------------------------
